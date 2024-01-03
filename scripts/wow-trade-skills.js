62c411219d3a14ef7ccc3bd3bc6fe4d2300e8e63
Mark Branscombe <spangoss@mac.com>
2024-01-03 21:05:51 +0000

    Abandon loading JSON files.

--- scripts/wow-trade-skills.js
+++ scripts/wow-trade-skills.js
@@ -15,29 +15,6 @@ app.filter("unique", function () {
     };
 });
 
-
-
-
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
-loadJSON(function (response) {
-    // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
-    console.log(actual_JSON);
-});
-
 app.controller('tradeSkillsCntrl', function ($scope) {
 
     $scope.showImportTab = false;
